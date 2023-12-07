74c41082e0fb3844d24b91e1f5c04511a0c0fdf0
vinicius webster <viniciuswebster99@gmail.com>
2023-12-07 17:05:37 -0300

    feat: add new colors variables

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -6,4 +6,6 @@ export const primaryFont = `'Roboto', sans-serif`;
 //colors
 export const primaryColor = '#0EA5E9';
 export const secundaryColor = '#10B981';
+export const darkFontColor = '#4B5563';
+export const mdFontColor = '#6B7280';
 export const white = '#fff';
