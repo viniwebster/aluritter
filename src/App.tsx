0d54a4487ded840ad90c5f22ed6008365fb4d8ec
vinicius webster <viniciuswebster99@gmail.com>
2023-12-09 19:46:43 -0300

    feat: add private and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import NotFound from './pages/NotFound'
 import Home from "./pages/Home"
 import { GlobalStyles } from "./styles/GlobalStyles"
 import SingUp from "./pages/SingUp"
 import SingIn from "./pages/SingIn"
 
+interface Props {
+  children: React.ReactElement
+}
+
+const PrivateRoute = ({ children } : Props) => {
+  if (!localStorage.getItem("access-token")) {
+    return <Navigate to="/singin" />;
+  }
+  return children;
+};
+
+const PublicRoute = ({ children } : Props) => {
+  if (localStorage.getItem("access-token")) {
+    return <Navigate to="/" />;
+  }
+  return children;
+};
+
 function App() {
   return (
     <BrowserRouter>
     <GlobalStyles />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/singup" element={<SingUp />} />
-        <Route path="/singin" element={<SingIn />} />
+        <Route path="/singup" element={<PublicRoute><SingUp /></PublicRoute>} />
+        <Route path="/singin" element={<PublicRoute><SingIn /></PublicRoute>} />
       </Routes>
     </BrowserRouter>
   )
