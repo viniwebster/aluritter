6736bd450563d0af38556f2580e113e0296ced30
vinicius webster <viniciuswebster99@gmail.com>
2023-12-13 14:35:49 -0300

    feat: method for alurite, using database from firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,18 @@ import styled from "styled-components";
 import Header from "../components/Header/Header";
 import Textarea from "../components/Textarea/Textarea";
 import Button from "../components/Button/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Alurite from "../components/Alurite/Alurite";
 import { darkFontColor } from "../styles/variables";
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import { v4 as uuidv4 } from "uuid";
+import { IAlurite } from "../interfaces/IAlurite";
+import { jwtDecode } from "jwt-decode";
+import { getAuth } from "firebase/auth";
 
 const StyledSection = styled.section`
   padding: 40px;
-  background-color: #E5E7EB;
+  background-color: #e5e7eb;
   min-height: calc(100vh - 64px);
 `;
 
@@ -37,24 +42,63 @@ const StyledForm = styled.form`
 `;
 
 const Home = () => {
-  const [alurite, setAlurite] = useState("")
+  const [alurite, setAlurite] = useState("");
+  const [messages, setMessages] = useState<IAlurite[]>([]);
+  const user = jwtDecode<{email: string}>(localStorage.getItem("access-token")!);
+
+  useEffect(() => {
+    getAuth();
+    onValue(ref(getDatabase(), "alurites"), (snapshot) => {
+      const data: IAlurite[] = [];
+      snapshot.forEach((registry) => {
+        data.push({
+          ...registry.val(),
+          id: registry.key,
+        });
+      });
+      setMessages(data);
+    });
+  }, []);
+
+  const onAlurite = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const db = getDatabase();
+    set(ref(db, `alurites/${uuidv4()}`), {
+      message: alurite,
+      email: user.email,
+      data: new Date().getTime(),
+      id: uuidv4()
+    })
+      .then(() => setAlurite(""))
+      .catch((error) => console.log(error));
+  };
 
   return (
     <>
-      <Header />
+      <Header email={user.email} />
       <StyledSection>
-        <StyledForm>
+        <StyledForm onSubmit={onAlurite}>
           <h2>Alurite agora mesmo...</h2>
-          <Textarea 
-            value={alurite}
-            setValue={setAlurite}
-          />
+          <Textarea value={alurite} setValue={setAlurite} />
           <div>
-            <p>Você ainda pode digitar 255 caracteres</p>
-            <Button text="aluritar" color="#0EA5E9"/>
+            {alurite.length < 255 ? (
+              <p>Você ainda pode digitar {255 - alurite.length} caracteres</p>
+            ) : (
+              <p>Você usou todos os caracteres</p>
+            )}
+            <Button text="aluritar" color="#0EA5E9" />
           </div>
         </StyledForm>
-        <Alurite />
+        {messages
+          .sort((msg1, msg2) => msg2.data - msg1.data)
+          .map((alurite) => (
+            <Alurite
+              key={alurite.id}
+              data={alurite.data}
+              message={alurite.message}
+              email={alurite.email}
+            />
+          ))}
       </StyledSection>
     </>
   );
