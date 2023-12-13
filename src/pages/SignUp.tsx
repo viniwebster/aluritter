4f0b3a74901cf44342ba9055f3667c2a40259dd7
vinicius webster <viniciuswebster99@gmail.com>
2023-12-13 14:33:03 -0300

    feat: signUp method with firebase service

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import Input from "../components/Input/Input";
+import styled from "styled-components";
+import Logo from "../components/Logo/Logo";
+import Button from "../components/Button/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { primaryColor } from "../styles/variables";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const StyledSection = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledForm = styled.form`
+  max-width: 400px;
+  padding: 0 32px;
+
+  h1 {
+    text-align: center;
+    margin-bottom: 18px;
+  }
+
+  input {
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin-top: 10px;
+    text-align: center;
+    font-size: 14px;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: ${primaryColor};
+  text-decoration: none;
+`;
+
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const onSignUp = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        user.getIdToken().then(jwt => localStorage.setItem("access-token", jwt))
+        setEmail("");
+        setPassword("");
+        navigate('/');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+      });
+  };
+
+  return (
+    <StyledSection>
+      <StyledForm onSubmit={onSignUp}>
+        <Logo />
+        <Input
+          value={email}
+          placeholder="email@exemplo.com"
+          onChange={(event) => setEmail(event)}
+          type="email"
+        />
+        <Input
+          value={password}
+          placeholder="senha"
+          onChange={(event) => setPassword(event)}
+          type="password"
+          minLengh={8}
+        />
+        <Button text="Criar uma nova conta" />
+        <p>
+          Já possui uma conta?
+          <StyledLink to={"/signin"}> Acesse agora!</StyledLink>
+        </p>
+      </StyledForm>
+    </StyledSection>
+  );
+};
+
+export default SignUp;
